5073395076b6b83ce7907dc262f772f91e4581f0
Kaue Miziara <kauemiziara@gmail.com>
2023-09-03 19:24:39 -0300

    Setup internal LED GPIO

--- main/esp32-obd-scan-sim.c
+++ main/esp32-obd-scan-sim.c
@@ -2,21 +2,28 @@
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <driver/gpio.h>
 #include <esp_bt.h>
 #include <esp_bt_main.h>
 #include <esp_err.h>
 #include <esp_log.h>
 #include <esp_system.h>
+#include <hal/gpio_types.h>
 #include <nvs.h>
 #include <nvs_flash.h>
 
 #define BT_TAG "BLUETOOTH_INIT"
 
+const gpio_num_t INTERNAL_LED = 2;
+
 void initialize_bluetooth(void);
+void initialize_gpio(void);
 
 void app_main(void)
 {
   initialize_bluetooth();
+
+  initialize_gpio();
 }
 
 void initialize_bluetooth(void)
@@ -44,4 +51,12 @@ void initialize_bluetooth(void)
     ESP_LOGE(BT_TAG, "Bluetooth controller enable failed, error code %s", esp_err_to_name(ret));
     return;
   }
+}
+
+void initialize_gpio(void)
+{
+  gpio_reset_pin(INTERNAL_LED);
+  gpio_set_direction(INTERNAL_LED, GPIO_MODE_OUTPUT);
+
+  gpio_set_level(INTERNAL_LED, 0);
 }
