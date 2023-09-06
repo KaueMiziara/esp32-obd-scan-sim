d47b452c69f30f566aa40bfd976559cae0818491
Kaue Miziara <kauemiziara@gmail.com>
2023-09-06 19:13:19 -0300

    Remove old files

--- include/bluetooth.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <esp_spp_api.h>
-
-void initialize_bluetooth(void);
-
-void spp_server_task(void* param);
-void spp_server_callback(esp_spp_cb_event_t event, esp_spp_cb_param_t* param);
