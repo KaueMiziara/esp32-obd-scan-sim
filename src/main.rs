77e1b826e20aa4d29a7d732ce546e5e6ef333f32
Kaue Miziara <kauemiziara@gmail.com>
2023-09-07 16:59:32 -0300

    Extract hard coded UUID to a constant

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use esp32_nimble::{utilities::BleUuid, BLEDevice};
 use esp_idf_sys as _;
-use esp32_nimble::{uuid128, BLEDevice};
 use log::{info, warn};
+
+const SERVICE_UUID: BleUuid = BleUuid::Uuid128([
+    0x2c, 0x05, 0xad, 0xa8, 0x69, 0x58, 0x4d, 0x58, 0x84, 0x51, 0xf5, 0x8a, 0x11, 0x06, 0x87, 0x9e,
+]);
 
 fn main() {
     esp_idf_sys::link_patches();
@@ -13,39 +17,27 @@ fn main() {
         ::log::info!("Client conencted");
 
         let params = (desc.conn_handle, 24, 48, 0, 60);
-        match server.update_conn_params(
-            params.0,
-            params.1, 
-            params.2, 
-            params.3, 
-            params.4,
-        ) {
+        match server.update_conn_params(params.0, params.1, params.2, params.3, params.4) {
             Ok(_) => info!("Connection parameters updated"),
-            Err(err) => warn!(
-                "Connection parameters update failed\nError: {:?}", err
-            ),
+            Err(err) => warn!("Connection parameters update failed\nError: {:?}", err),
         }
 
         match ble_device.get_advertising().start() {
             Ok(_) => info!("Start advertising"),
-            Err(err) => warn!(
-                "Failed to start advertising\nError: {:?}", err
-            ),
+            Err(err) => warn!("Failed to start advertising\nError: {:?}", err),
         }
     });
 
-    let _service = server.create_service(uuid128!("2c05ada8-6958-4d58-8451-f58a1106879e"));
+    let _service = server.create_service(SERVICE_UUID);
 
     let ble_advertising = ble_device.get_advertising();
     ble_advertising
         .name("ESP32-OBD-SIM")
-        .add_service_uuid(uuid128!("2c05ada8-6958-4d58-8451-f58a1106879e"));
+        .add_service_uuid(SERVICE_UUID);
 
     match ble_advertising.start() {
         Ok(_) => info!("Start advertising"),
-        Err(err) => warn!(
-            "Failed to start advertising\nError: {:?}", err
-        ),
+        Err(err) => warn!("Failed to start advertising\nError: {:?}", err),
     }
 
     loop {
